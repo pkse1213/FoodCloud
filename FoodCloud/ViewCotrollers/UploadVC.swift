8d1e7f43aa5e5ac5d093cb98ba6d8baa00c3712b
박세은 <pkse1213@naver.com>
2018-11-17 02:06:10 +0900

    업로드 뷰

--- FoodCloud/ViewCotrollers/UploadVC.swift
+++ FoodCloud/ViewCotrollers/UploadVC.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class UploadVC: UIViewController {
 
+    @IBOutlet weak var imgClV: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupClV()
+    }
+    private func setupClV() {
+        self.imgClV.delegate = self
+        self.imgClV.dataSource = self
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UploadVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 }
